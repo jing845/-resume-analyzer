29ce8b6e6d53037083941e9fbc09ed7b7d0d33d9
jing845 <2421663551@qq.com>
2026-04-06 17:30:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-# config.py
-API_KEY = "sk-agwmermcbbwuxljzxvkfpclbsytfgoxhetbfwgtlcbgkofza"
+import os
+
+API_KEY = os.environ.get("API_KEY")
 BASE_URL = "https://api.siliconflow.cn/v1"
 MODEL = "Qwen/Qwen2.5-72B-Instruct"
